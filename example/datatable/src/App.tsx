d1c7821b0e518ed9e398e77297c84236086e00a2
中島浩二 <knaka0209@gmail.com>
2024-09-18 06:31:44 +0900

    up, 0918a2

--- example/datatable/src/App.tsx
+++ example/datatable/src/App.tsx
@@ -3,24 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 //
 import Home from './client/home';
 import About from './client/about';
-import AlertDialog from './client/AlertDialog';
-import Button from './client/Button';
-import Card from './client/Card';
-import Calendar from './client/Calendar';
-import CheckBox from './client/CheckBox';
 import DataTable from './client/DataTable';
-import DatePicker from './client/DatePicker';
-import Dialog from './client/Dialog';
-import Form from './client/Form';
-import Input from './client/Input';
-import NavigationMenu from './client/NavigationMenu';
-import Progress from './client/Progress';
-import Radio from './client/Radio';
-import Select from './client/Select';
-import Sheet from './client/Sheet';
-import Switch from './client/Switch';
-import Table from './client/Table';
-import TextArea from './client/TextArea';
 import Test1 from './client/Test1';
 import Test2 from './client/Test2';
 
